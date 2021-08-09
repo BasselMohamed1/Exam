1711aea8008d709513cb6f43726052c548b3cc29
BasselMohamed1 <85442878+BasselMohamed1@users.noreply.github.com>
2021-08-09 19:27:27 +0200

    Create script.js

---
+++ Assignment/js/script.js
@@ -0,0 +1,11 @@
+var namesj = ["Jayden" ,  "James" , "Jessica" ];
+
+for (var name in namesj) {
+	console.log("Goodbye " + namesj[name]);
+}
+
+var names = [ "Mike" , "Elena" , "Noah" ];
+
+for (var name in names) {
+	console.log("hello " + names[name]);
+}
