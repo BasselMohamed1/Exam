21a8ce4d20d20d368a910b541e7df4b785fbe368
BasselMohamed1 <85442878+BasselMohamed1@users.noreply.github.com>
2021-08-09 19:57:15 +0200

    Update script.js

--- Assignment/js/script.js
+++ Assignment/js/script.js
@@ -1,11 +1,13 @@
-var namesj = ["Jayden" ,  "James" , "Jessica" ];
-
-for (var name in namesj) {
-	console.log("Goodbye " + namesj[name]);
-}
-
-var names = [ "Mike" , "Elena" , "Noah" ];
-
-for (var name in names) {
-	console.log("hello " + names[name]);
-}
+(function () {
+	var names = ['Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim'];
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i];
+		var firstLetter = name.charAt(0).toLowerCase();
+		if (firstLetter === 'j') {
+			console.log("GoodBye " + name);
+		} else {
+			console.log("Hello " + name);
+		
+		}
+	}
+})();
