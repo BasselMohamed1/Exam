c463f203e7e362d27fc5dcc2dd8097ea08cb66aa
BasselMohamed1 <85442878+BasselMohamed1@users.noreply.github.com>
2021-08-09 20:10:35 +0200

    Update script.js

--- Assignment/js/script.js
+++ Assignment/js/script.js
@@ -1,12 +1,31 @@
+var helloSpeaker = {
+	speakword : "Hello",
+};
+
+(function () {
+	helloSpeaker.speak = function (username){
+		return (helloSpeaker.speakword+""+username);
+	}
+})();
+
+var byeSpeaker = {
+	speakword : "Hello",
+};
+
+(function () {
+	byeSpeaker.speak = function (username){
+		return (byeSpeaker.speakword+""+username);
+	}
+})();
 (function () {
 	var names = ['Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim'];
 	for (var i = 0; i < names.length; i++) {
 		var name = names[i];
 		var firstLetter = name.charAt(0).toLowerCase();
 		if (firstLetter === 'j') {
-			console.log("GoodBye " + name);
+			console.log(byeSpeaker.speak(name));
 		} else {
-			console.log("Hello " + name);
+			console.log(helloSpeaker.speak(name));
 		
 		}
 	}
